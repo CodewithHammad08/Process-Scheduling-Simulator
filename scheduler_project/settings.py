0d58880b08375675fb01f6978856dfc6b02fb628
Hammad Dalvi <hammaddalvi08@gmail.com>
2026-03-16 17:21:03 +0530

    feat: Add initial Django project settings, including `scheduler_app` and template/static file configurations.

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7lf9x1yy=#@dskpk9d3(go!y1-n9%$@^ey$)mn%zz84ib$^v#p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
